b1908e2e0d7cf819ef9ab5a19c7fe714279bbda9
EcSolticia <ecsolticia@gmail.com>
2025-07-07 16:59:11 +0600

    close #8: Implement syntax-highlighting for code blocks in the HTML docs

--- docs/build-docs.nix
+++ docs/build-docs.nix
@@ -5,7 +5,7 @@ pkgs.writeShellScriptBin "build-docs" ''
 
   cat ./docs/docs_prepend.txt ./docs/DOCS.md ./docs/docs_append.txt > ./docs/tmp_docs.md
 	  
-  pandoc --from markdown --to html5 --standalone --css=docs_style.css --toc --number-sections --no-highlight -o ./docs/output_docs.html ./docs/tmp_docs.md
+  pandoc --from markdown --to html5 --standalone --css=docs_style.css --toc --number-sections --highlight-style=zenburn -o ./docs/output_docs.html ./docs/tmp_docs.md
   rm ./docs/tmp_docs.md
 
   echo 'Done building docs. See docs/output_docs.html'
